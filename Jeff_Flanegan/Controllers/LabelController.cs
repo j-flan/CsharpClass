6227cd3c3e518f2257f819584b8c0339fe1e20cc
j-flan <jhflanegan@gmail.com>
2020-02-24 18:51:55 -0700

    mailingLabel

---
+++ Jeff_Flanegan/Controllers/LabelController.cs
@@ -0,0 +1,21 @@
+﻿using Jeff_Flanegan.Models;
+using System.Web.Mvc;
+
+namespace Jeff_Flanegan.Controllers
+{
+    public class LabelController : Controller
+    {
+        public ActionResult Index()
+        {
+            LabelViewModel model = new LabelViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(LabelViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Jeff_Flanegan/Models/LabelViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Jeff_Flanegan.Models
+{
+    public class LabelViewModel
+    {
+        public Label Label { get; set; }
+
+        public SelectList StateList
+        {
+            get
+            {
+
+                List<ListItem> states = new List<ListItem>();
+                //Add some items to the list
+                states.Add(new ListItem("Colorado", "CO"));
+                states.Add(new ListItem("Michigan", "MI"));
+                states.Add(new ListItem("Oregon", "OR"));
+                states.Add(new ListItem("Texas", "TX"));
+                states.Add(new ListItem("Illinois", "IL"));
+
+                //create a new SelectList passing in the list of ListItem, Value as the value field, 
+                //Description as the description field
+                return new SelectList(states, "Value", "Description");
+            }
+        }
+
+
+        public LabelViewModel()
+        {
+            Label = new Label();
+        }
+    }    
+
+    public class ListItem
+    {
+        public string Description { get; set; }
+        public string Value { get; set; }
+
+        public ListItem(string description, string value)
+        {
+            Description = description;
+            Value = value;
+        }
+    }
+}
